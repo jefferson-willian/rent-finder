3a4d622ecb57b35cf380b23a2d18b8e5444491da
Jefferson Monteiro <jefferson.willian.monteiro@gmail.com>
2021-04-14 21:21:19 -0300

    Dummy test with Playwright & Heroku.

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+const cheerio = require('cheerio');
+const playwright = require('playwright');
+
+let myBrowser = null;
+
+playwright['chromium'].launch()
+  .then(browser => {
+    myBrowser = browser;
+    return browser.newContext();
+  })
+  .then(context => context.newPage())
+  .then(page => page.goto('http://whatsmyuseragent.org/'))
+  .then(response => response.text())
+  .then(html => {
+    const $ = cheerio.load(html);
+    console.log($('.ip-address').text().trim());
+  })
+  .catch(err => console.log(err))
+  .finally(() => myBrowser.close());
