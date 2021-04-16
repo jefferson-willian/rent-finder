7de251e99e3136760ce23a8e686d1151e9df9031
Jefferson Monteiro <jefferson.willian.monteiro@gmail.com>
2021-04-15 22:45:31 -0300

    VivaReal dummy test.

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const cheerio = require('cheerio');
 const playwright = require('playwright');
+const fs = require('fs');
 
-let myBrowser = null;
+const URL = 'https://www.vivareal.com.br/aluguel/minas-gerais/belo-horizonte/bairros/grajau/apartamento_residencial/#ordenar-por=preco-total:DESC&preco-ate=3500&preco-desde=2000&preco-total=sim&tipos=apartamento_residencial,cobertura_residencial';
+
+function getResults(html) {
+  const $ = cheerio.load(html);
+  var keepGettingResults = true;
+
+  return $('.results-list').children().filter((i, node) => {
+    keepGettingResults = keepGettingResults
+      ? node.attribs['data-type'] == 'property' : false;
+    return keepGettingResults;
+  })
+  .map((i, node) => $(node).find('.property-card__content-link').attr('href'))
+  .toArray();
+}
+
+var myBrowser;
 
 playwright['chromium'].launch()
   .then(browser => {
@@ -9,11 +25,10 @@ playwright['chromium'].launch()
     return browser.newContext();
   })
   .then(context => context.newPage())
-  .then(page => page.goto('http://whatsmyuseragent.org/'))
+  .then(page => page.goto(URL))
   .then(response => response.text())
   .then(html => {
-    const $ = cheerio.load(html);
-    console.log($('.ip-address').text().trim());
+    console.log("Total: " + getResults(html).length);
   })
   .catch(err => console.log(err))
   .finally(() => myBrowser.close());
