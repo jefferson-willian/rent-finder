80ede575ee94f713b0a6671ddaba3a301007e26b
Jefferson Monteiro <jefferson.willian.monteiro@gmail.com>
2021-04-16 23:33:52 -0300

    Extract scrapper logic.

--- main.js
+++ main.js
@@ -1,27 +1,13 @@
 const browser = require('./browser.js');
-const cheerio = require('cheerio');
+const scrappers = require('./scrappers.js');
 
 const URL = 'https://www.vivareal.com.br/aluguel/minas-gerais/belo-horizonte/bairros/grajau/apartamento_residencial/#ordenar-por=preco-total:DESC&preco-ate=3500&preco-desde=2000&preco-total=sim&tipos=apartamento_residencial,cobertura_residencial';
 
-function getResults(html) {
-  const $ = cheerio.load(html);
-  var keepGettingResults = true;
-
-  return $('.results-list').children().filter((i, node) => {
-    keepGettingResults = keepGettingResults
-      ? node.attribs['data-type'] == 'property' : false;
-    return keepGettingResults;
-  })
-  .map((i, node) => $(node).find('.property-card__content-link').attr('href'))
-  .toArray();
-}
-
 const myBrowser = browser.newBrowser();
+const vivarealScrapper = scrappers.newVivaRealScrapper(myBrowser);
 
 myBrowser.launch()
-  .then(() => myBrowser.gotoAndWaitForRequest(URL, /.*property\-detail\.js/))
-  .then(html => {
-    console.log("Total: " + getResults(html).length);
-  })
+  .then(() => vivarealScrapper.extractFrom(URL))
+  .then(result => console.log("Result: " + result))
   .then(() => myBrowser.close())
   .catch(err => console.log(err));
---
+++ scrappers.js
@@ -0,0 +1,30 @@
+const cheerio = require('cheerio');
+
+
+class VivaRealScrapper {
+  constructor(browser) {
+    this.browser_ = browser;
+  }
+
+  extractFrom(url) {
+    return this.browser_.gotoAndWaitForRequest(url, /.*property\-detail\.js/)
+      .then(html => getResults(html));
+  }
+
+  getResults_(html) {
+    const $ = cheerio.load(html);
+    var keepGettingResults = true;
+
+    return $('.results-list').children().filter((i, node) => {
+      keepGettingResults = keepGettingResults
+        ? node.attribs['data-type'] == 'property' : false;
+      return keepGettingResults;
+    })
+    .map((i, node) => $(node).find('.property-card__content-link').attr('href'))
+    .toArray();
+  }
+}
+
+exports.newVivaRealScrapper = function (browser) {
+  return new VivaRealScrapper(browser);
+}
