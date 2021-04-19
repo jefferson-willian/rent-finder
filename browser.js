54cd34f7738a775bfc6d0749005946ca90109e60
Jefferson Monteiro <jefferson.willian.monteiro@gmail.com>
2021-04-18 21:06:26 -0300

    Add multi-thread support for fetching HTML pages.

--- browser.js
+++ browser.js
@@ -3,7 +3,7 @@ const playwright = require('playwright');
 class Browser {
   constructor() {
     this.browser_ = null;
-    this.page_ = null;
+    this.context_ = null;
     this.engine_ = 'chromium';
   }
 
@@ -11,15 +11,19 @@ class Browser {
     return playwright[this.engine_].launch()
       .then(browser => this.browser_ = browser)
       .then(() => this.browser_.newContext())
-      .then(context => context.newPage())
-      .then(page => this.page_ = page)
-      .then(() => true);
+      .then(context => this.context_ = context);
   }
 
   gotoAndWaitForRequest(url, request) {
-    return this.page_.goto(url)
-      .then(() => this.page_.waitForRequest(request))
-      .then(() => this.page_.content());
+    var page = null;
+
+    return this.context_.newPage()
+      .then(p => page = p)
+      .then(() => page.goto(url))
+      .then(() => page.waitForRequest(request))
+      .then(() => page.content())
+      .then(content => Promise.all([Promise.resolve(content), page.close()]))
+      .then(result => result[0]);
   }
 
   close() {
